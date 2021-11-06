116a5c8762cb98e869009a42302d5aedbdeab172
sherlock <quangkhai28999@gmail.com>
2021-11-06 23:21:56 +0700

    finish

--- project 1/wiki/encyclopedia/urls.py
+++ project 1/wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry_page, name="entry_page"),
     path("createnewpage", views.create_new_page, name="create_new_page"),
-
+    path("editpage", views.edit_page, name="edit_page"),
+    path("random", views.random_page, name="random_page"),
 ]
--- project 1/wiki/encyclopedia/views.py
+++ project 1/wiki/encyclopedia/views.py
@@ -1,7 +1,12 @@
+from django.http import response
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import markdown
+import urllib
+import random
+
 from . import util
+from django.urls import reverse
 
 
 def index(request):
@@ -20,9 +25,13 @@ def entry_page(request, title):
         markdown_file = util.get_entry(title.capitalize()) # try capitalize title then search
         if markdown_file == None: 
             return HttpResponse(f"No results found for {title}")
-    md = markdown.Markdown(extensions=['pymdownx.emoji'])
+    md = markdown.Markdown(extensions=['pymdownx.emoji']) # allow markdown emojis translate to html file
     html_file = md.convert(markdown_file)
-    return HttpResponse(html_file)
+
+    return render(request, "encyclopedia/entry_page.html", {
+        'html_file' : html_file
+    })
+    
     
 def search_result(request, search_key):
     entries = []
@@ -34,12 +43,30 @@ def search_result(request, search_key):
     })
 
 def create_new_page(request) :
-    if request.method == "POST" :
+    if request.method == "POST":
         title = request.POST['title']
+        if title in util.list_entries() and request.POST['isUpdate'] == "":
+            return render(request,"encyclopedia/createnewpage.html", {
+                'message': f"page {title} is already exist." 
+            })
         content = request.POST['content']
         util.save_entry(title, content)
-        return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-        })
-
+        return HttpResponseRedirect(f"wiki/{title}")
+    # case you want to create new page
     return render(request, "encyclopedia/createnewpage.html")
+
+def edit_page(request) :
+    title = request.META['HTTP_REFERER']
+    res = str.rsplit(title, '/')[-1]
+    for title in util.list_entries():
+        if res == urllib.parse.quote(title) :
+            isUpdate = True;
+            content = util.get_entry(title)
+            return render(request, "encyclopedia/createnewpage.html", {
+                'title': title, 'content': content, 'isUpdate': isUpdate
+            })
+    return HttpResponseRedirect(reverse("create_new_page"))
+
+def random_page(request):
+    title = random.choice(util.list_entries())
+    return HttpResponseRedirect(f"wiki/{title}")
